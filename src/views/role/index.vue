7329a44ec07010995b793488e6bc886286f4116a
zks <zks13218919255@163.com>
2023-12-12 22:22:52 +0800

    重置表单必须有prop属性哪怕没有字段

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -40,7 +40,7 @@
         <el-form-item prop="name" label="角色名称">
           <el-input v-model="roleForm.name" style="width:300px" size="mini" />
         </el-form-item>
-        <el-form-item label="启用">
+        <el-form-item label="启用" prop="state">
           <!-- 如果不需要校验 就不需要写 prop属性 -->
           <el-switch v-model="roleForm.state" :active-value="1" :inactive-value="0" size="mini" />
         </el-form-item>
